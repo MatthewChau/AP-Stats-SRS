ddd1e9ba391b14caf97e3cff3cd64939ac0e0833
Matthew <MatthewChauA@gmail.com>
2019-04-20 10:32:40 -0400

    Throw out repeats
    Checks new randomly generated codes against each one of the previous ones. Iif a repeat is found, that array index is overwritten and the main for loop runs another time to do that.

--- Test/src/app/App.java
+++ Test/src/app/App.java
@@ -9,7 +9,8 @@ public class App {
 
         Random _generator = new Random();
 
-        int[] heights = {65, 63, 58, 71, 56, 64, 64, 63, 65, 
+        int[] heights = {
+            65, 63, 58, 71, 56, 64, 64, 63, 65, 66,
             66, 70, 51, 55, 57, 64, 63, 69, 59, 60,
             62, 62, 67, 66, 64, 63, 72, 68, 66, 63,
             62, 65, 56, 59, 66, 68, 62, 61, 58, 59,
@@ -18,19 +19,47 @@ public class App {
             61, 64, 68, 65, 66, 69, 59, 70, 70, 60,
             60, 65, 68, 67, 63, 61, 58, 68, 65, 69};
 
-        int[] _codes = new int[10];
+        int SAMPLE_SIZE = 10;
 
-        int[] _values = new int[10];
+        int[] _codes = new int[SAMPLE_SIZE];
 
-        double[] _partials = new double[10];
+        int[] _values = new int[SAMPLE_SIZE];
+
+        double[] _partials = new double[SAMPLE_SIZE];
+
+        boolean reset = false;
         
-        for (int i=0; i < 10; i++) {
+        for (int i=0; i < SAMPLE_SIZE; i++) {
+
+            reset = false;
+
             _codes[i] = _generator.nextInt(80);
-            System.out.println("code " + i + " " + _codes[i]);
+            
 
-            _values[i] = heights[_codes[i]];
+            // Throw out repeats
+            for (int p=i; p > 0; p--) {
+                if (_codes[i] == _codes[p - 1]) {
+                    i--;
+                    reset = true;
+                    System.out.println("Repeat");
+                }
+            }
 
-            System.out.println(" value " + _values[i]);
+            if (!reset) {
+                _values[i] = heights[_codes[i]];
+                System.out.println("code " + i + " " + _codes[i]);
+                System.out.println(" value " + _values[i]);
+            }
+        }
+
+        for (int l=0; l < SAMPLE_SIZE; l++) {
+            System.out.print(_codes[l] + " ");
+        }
+
+        System.out.println(" ");
+
+        for (int q=0; q < SAMPLE_SIZE; q++) {
+            System.out.print(_values[q] + " ");
         }
 
         int sum = IntStream.of(_values).sum();
@@ -38,12 +67,12 @@ public class App {
 
         System.out.println("Mean " + mean);
 
-        for (int i=0; i < 10; i++) {
+        for (int i=0; i < SAMPLE_SIZE; i++) {
             _partials[i] = Math.pow(_values[i] - mean, 2);
         }
 
         double partialSum = DoubleStream.of(_partials).sum();
-        double stdDev = partialSum / 9;
+        double stdDev = partialSum / (SAMPLE_SIZE - 1);
 
         System.out.println("Standard Deviation " + stdDev);
     }
